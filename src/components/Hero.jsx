38aeb2ef624bd24a00e50d633d87fbd562527af7
Ayush Gupta <90502624+Predator792002@users.noreply.github.com.>
2024-05-01 14:43:10 +0530

    changed link to the github

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
         <button
           type='button'
           onClick={() =>
-            window.open("https://github.com/TidbitsJS/Summize", "_blank")
+            window.open("https://github.com/Predator792002/project_ai_summarizer", "_blank")
           }
           className='black_btn'
         >
